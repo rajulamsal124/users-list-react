d429a848eb1723637aeecc806d1df0e527703591
Raju Lamsal <raju.eversoft@gmail.com>
2020-04-30 14:26:28 +0545

    monster app-udemy react course

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import "./App.css";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        {
+          name: "Frankenstien",
+          id: "asc1",
+        },
+        {
+          name: "Darcula",
+          id: "asc2",
+        },
+        {
+          name: "Zombie",
+          id: "asc3",
+        },
+      ],
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
